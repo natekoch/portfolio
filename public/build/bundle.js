90409e8fa17e13853cfa4a65adbf2563a3fac152
Nate Koch <31897172+natekoch@users.noreply.github.com>
2022-04-23 13:30:48 -0700

    Initial public subtree commit

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -1283,11 +1283,14 @@ var app = (function () {
     	let t0;
     	let div2;
     	let div1;
-    	let h1;
+    	let img;
+    	let img_src_value;
     	let t1;
+    	let h1;
+    	let t2;
     	let span;
-    	let t3;
     	let t4;
+    	let t5;
     	let p;
 
     	const block = {
@@ -1298,22 +1301,30 @@ var app = (function () {
     			t0 = space();
     			div2 = element("div");
     			div1 = element("div");
+    			img = element("img");
+    			t1 = space();
     			h1 = element("h1");
-    			t1 = text("Hello there! I am ");
+    			t2 = text("Hello there! I am ");
     			span = element("span");
     			span.textContent = "Nate Koch";
-    			t3 = text(". Welcome to my site.");
-    			t4 = space();
+    			t4 = text(". Welcome to my site.");
+    			t5 = space();
     			p = element("p");
     			p.textContent = "Enjoy your stay, please check out all of the available pages. There are photos to see, programming projects to dive into, a blog to read, and a page all about me. Use the contact page to get in contact with me for whatever your need may be.";
     			attr_dev(div0, "class", "rounded-box hero-overlay bg-opacity-20");
     			add_location(div0, file$1, 2, 8, 120);
+    			attr_dev(img, "class", "mask mask-circle mb-3 mx-auto");
+    			if (!src_url_equal(img.src, img_src_value = "00043261/07EC667B-4464-4ADB-BB23-8B5B0B322D0A_1_105_c.jpeg")) attr_dev(img, "src", img_src_value);
+    			set_style(img, "max-height", "250px");
+    			set_style(img, "max-width", "250px");
+    			attr_dev(img, "alt", "profile picture");
+    			add_location(img, file$1, 6, 12, 350);
     			attr_dev(span, "class", "text-secondary");
-    			add_location(span, file$1, 5, 66, 346);
-    			attr_dev(h1, "class", "mb-5 text-5xl font-bold");
-    			add_location(h1, file$1, 5, 12, 292);
+    			add_location(span, file$1, 7, 78, 605);
+    			attr_dev(h1, "class", "mb-5 text-4xl sm:text-5xl font-bold");
+    			add_location(h1, file$1, 7, 12, 539);
     			attr_dev(p, "class", "mb-5");
-    			add_location(p, file$1, 6, 12, 430);
+    			add_location(p, file$1, 8, 12, 689);
     			attr_dev(div1, "class", "max-w-md");
     			add_location(div1, file$1, 4, 10, 257);
     			attr_dev(div2, "class", "hero-content text-center text-neutral-content");
@@ -1333,11 +1344,13 @@ var app = (function () {
     			append_dev(div3, t0);
     			append_dev(div3, div2);
     			append_dev(div2, div1);
+    			append_dev(div1, img);
+    			append_dev(div1, t1);
     			append_dev(div1, h1);
-    			append_dev(h1, t1);
+    			append_dev(h1, t2);
     			append_dev(h1, span);
-    			append_dev(h1, t3);
-    			append_dev(div1, t4);
+    			append_dev(h1, t4);
+    			append_dev(div1, t5);
     			append_dev(div1, p);
     		},
     		p: noop,
