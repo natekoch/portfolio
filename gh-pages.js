92543b2ff73de97abd5ebad836d647c9b205163c
Nate Koch <31897172+natekoch@users.noreply.github.com>
2022-04-23 14:18:28 -0700

    gh-pages update

--- gh-pages.js
+++ gh-pages.js
@@ -1,17 +1,17 @@
-import { publish } from 'gh-pages';
+let ghpages = require('gh-pages');
 
-publish(
- '/public', // path to public directory
- {
-  branch: 'gh-pages',
-  repo: 'https://github.com/el3um4s/memento-sveltekit-and-github-pages.git', // Update to point to your repository
-  user: {
-   name: 'Nate Koch',
-   email: 'nkoch@jaaku.xyz'
-  },
-  dotfiles: true
-  },
-  () => {
-   console.log('Deploy Complete!');
-  }
+ghpages.publish(
+    '/public', // path to public directory
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/natekoch/portfolio.git',
+        user: {
+            name: 'Nate Koch',
+            email: 'nkoch@jaaku.xyz'
+        },
+        dotfiles: true
+    },
+    () => {
+    console.log('Deploy Complete!');
+    }
 );
