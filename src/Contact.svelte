9c826782bb5f6609331987d8d2a61e11eb6c7508
Nate Koch <31897172+natekoch@users.noreply.github.com>
2022-04-23 23:12:50 -0700

    updated styling of contact page

--- src/Contact.svelte
+++ src/Contact.svelte
@@ -1,10 +1,13 @@
 <body>
     <div class="grid grid-cols-1 grid-rows-2 grid-flow-row-dense place-items-center gap-0 h-screen">
         <div class="text-center">
-        <h1 class="text-bold text-2xl text-white">Get in contact with me.</h1>
-        <a href="mailto:nkoch@jaaku.xyz">
-            <button class="btn btn-secondary btn-sm md:btn-md lg:btn-lg m-4">Email Me</button> 
-        </a>
-    </div>
+            <h1 class="text-bold text-2xl text-secondary">Get in contact with me.</h1>
+            <h2 class="text-bold text-xl text-accent">Press the button below to send me an email.</h2>
+            <div class="tooltip tooltip-bottom tooltip-secondary" data-tip="nkoch@jaaku.xyz">
+                <a href="mailto:nkoch@jaaku.xyz">
+                    <button class="btn btn-secondary btn-sm md:btn-md lg:btn-lg m-4">Email Me</button> 
+                </a>
+            </div>
+        </div>
     </div>
 </body>
